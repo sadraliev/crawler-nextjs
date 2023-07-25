e9a503349635ccb7c29e0dbbef3291e0bef53e7c
sadraliev <a.sadraliev@gmail.com>
2023-07-25 16:47:56 +0600

    fix: build issues

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import data from "../../../public/results.json";
+import data from "@public/results.json";
 
 export async function GET(req: Request) {
   return NextResponse.json(data);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 import { Providers } from "@/components/Providers";
-import { configuration } from "@/config/configuration";
 import { scrapedData, Detail } from "@/interfaces/api.interface";
-import { GetStaticProps } from "next";
 
-interface MyComponentProps {
-  data: any; // Вы можете типизировать это на ваше усмотрение
-}
-import { useEffect } from "react";
+import okveds from "@public/results.json";
 
 async function getData(url: string): Promise<scrapedData[]> {
   const res = await fetch(url);
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
 
-  // Recommendation: handle errors
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
 
@@ -37,28 +28,12 @@ function DetailComponent({ detail }: { detail: Detail }) {
   );
 }
 
-function RenderScrapedData({ data }: { data: scrapedData }) {
-  return (
-    <div>
-      <h2>{data.sectionName}</h2>
-      <a href={data.sectionURL}>{data.sectionType}</a>
-      <ul>
-        {data.details.map(detail => (
-          <DetailComponent key={detail.code} detail={detail} />
-        ))}
-      </ul>
-    </div>
-  );
-}
 export default async function Home() {
-  const baseUrl = configuration.getUrl();
-  const response = await getData(baseUrl);
-
   return (
     <Providers>
       <div className="container mx-auto">
         <h1 className="text-center 3xl">Server Side Page</h1>
-        {response.map(okved => {
+        {okveds.map(okved => {
           return (
             <div className="my-3" key={okved.sectionName}>
               <div className="flex justify-between">
